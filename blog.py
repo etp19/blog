4692ece6112f7c01ca4eef57d6206e4c07c8c3ba
torrespe <torrespe@mail.lcc.edu>
2017-01-11 18:03:44 -0500

    Stage 2

--- blog.py
+++ blog.py
@@ -1,9 +1,17 @@
 import os
 import re
 from string import letters
+import user_and_password
+import security
 import webapp2
 import jinja2
 from google.appengine.ext import db
+
+not_valid_pass = "That wasn't a valid password."
+not_valid_user = "That's not a valid username."
+not_valid_email = "That's not a valid email."
+pass_no_match = "Your passwords didn't match."
+user_exist = "This user already exist"
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
@@ -35,10 +43,16 @@ class BlogDB(db.Model):
         return render_str("post.html", content = self)
 
 
+class UsersDB(db.Model):
+    username = db.StringProperty(required=True)
+    user_email = db.StringProperty()
+    user_password = db.StringProperty(required=True)
+
+
 class FrontBlog(BlogHandler):
     def get(self):
         posts = db.GqlQuery("select * from BlogDB order by created desc limit 10")
-        self.render("blog.html", posts = posts)
+        self.render("blog.html", posts=posts)
 
 
 class SeePost(BlogHandler):
@@ -71,14 +85,75 @@ class NewPost(BlogHandler):
             self.render("newpost.html", blog_title=post_title, blog_content=post_content, error=error)
 
 
+class SignUp(BlogHandler):
+    def get(self):
+        self.render("signup.html")
+
+
+    def post(self):
+        have_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        params = dict(username=username, email=email)
+
+        if not user_and_password.validate_user(username):
+            params['error_username'] = not_valid_user
+            have_error = True
+
+        if not user_and_password.validate_password(password):
+            params['error_password'] = not_valid_pass
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = pass_no_match
+            have_error = True
+
+        if not user_and_password.validate_email(email):
+            params['error_email'] = not_valid_email
+            have_error = True
+
+        users = db.GqlQuery("select * from UsersDB where username = :1", username).get() #take the user info from the database
+
+        if not users: #see if exist user.
+            secure_pw = security.make_pw_hash(username, password) #hash the password and store it in secure_pw
+            put_info = UsersDB(username=username, user_email=email, user_password=secure_pw) #put the information in database
+            put_info.put()
+        else:
+            params['error_exist'] = user_exist
+            have_error = True
+
+        if have_error:
+            self.render("signup.html", **params)
+
+        else:
+            self.response.headers['Content-Type'] = 'text/html'
+            user_id = 0
+            id_cookie_srt = self.request.cookies.get('user_id')
+            if id_cookie_srt:
+                cookie_val = security.test_security(id_cookie_srt)
+                if cookie_val:
+                    user_id = int(cookie_val)
+
+            user_id += 1
+            new_cookie_val = security.make_hash(str(user_id))
+            self.response.headers.add_header('Set-Cookie', 'user_id=%s' % new_cookie_val)
+            self.render("welcome.html", username=username)
+
+
 class MainPage(BlogHandler):
     def get(self):
         self.render("index.html")
+
+
+
+
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/blog/?', FrontBlog),
                                ('/blog/([0-9]+)', SeePost),
                                ('/blog/newpost', NewPost),
+                               ('/signup', SignUp)
                                ],
                               debug=True)
---
+++ security.py
@@ -0,0 +1,54 @@
+import hashlib
+import hmac
+import random
+import string
+
+x = hashlib.sha256('udacity')
+SECRET = '+d2d2dfrente'
+
+
+def hash_srt(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+
+def make_hash(s):
+    return "%s|%s" % (s, hash_srt(s))
+
+
+def test_security(h):
+    val = h.split('|')[0]
+    if h == make_hash(val):
+        return val
+
+
+# implement the function make_salt() that returns a string of 5 random
+# letters use python's random module.
+# Note: The string package might be useful here.
+
+def make_salt():#this is the function to make ramdom letters called salt.
+    return ''.join(random.choice(string.letters) for i in range(5))
+
+
+#print make_salt()
+
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()#call the function make_salt and store the value in salt
+    h = hashlib.sha256(name + pw + salt).hexdigest() #In this function we join the name with the pw and also we add the salt.
+    return '%s,%s' % (h, salt)  #after that we take the salt only and h which is encrypt version of everything and return it.
+
+
+def valid_pw(name, pw, h): #this function test if the username and password are good
+    salt = h.split(",")[1]
+    return h == make_pw_hash(name,pw,salt)
+
+h = make_pw_hash('spez', 'hunter2')
+print valid_pw('spez', 'hunter2', h)
+#print make_pw_hash('spez', 'hunter2','PEhRn')
+
+
+
+#print make_pw_hash("Eduardo", "water875")
+print make_hash("14")
+print test_security("14|2c66b804f0e51f85163afed94161ceef")
